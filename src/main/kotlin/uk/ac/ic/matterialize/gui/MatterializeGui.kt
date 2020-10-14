bb5a345704e8b0613e30195609922e8501ee6332
Raihaan Rahman <raihaan2000@gmail.com>
2020-10-14 12:19:36 +0100

    SEGP-26 Added a webcamViewController class and injected it into the webcamview class

--- src/main/kotlin/uk/ac/ic/matterialize/gui/MatterializeGui.kt
+++ src/main/kotlin/uk/ac/ic/matterialize/gui/MatterializeGui.kt
@@ -2,6 +2,7 @@ package uk.ac.ic.matterialize.gui
 
 import javafx.embed.swing.SwingFXUtils
 import javafx.scene.control.Label
+import javafx.scene.image.ImageView
 import javafx.scene.image.WritableImage
 import javafx.stage.FileChooser
 import org.bytedeco.javacv.Frame
@@ -16,34 +17,66 @@ import kotlin.system.exitProcess
 
 class MatterializeApp : App(WebcamView::class) {
 
+    private val webcamViewController: WebcamViewController by inject()
+
     override fun stop() {
+        webcamViewController.joinThreads()
         exitProcess(0)
     }
 }
 
+class WebcamViewController: Controller() {
+    private val java2DFrameConverter: Java2DFrameConverter = Java2DFrameConverter()
+    val grabber = OpenCVFrameGrabber(0)
+
+    fun applyNewBackground(inputValue: String) {
+        println("Image '$inputValue' has been selected")
+    }
+
+    fun joinThreads() {
+        // terminate all threads
+    }
+
+    fun initialiseWebCamThread(image: ImageView) {
+        val grabberConverter = OpenCVFrameConverter.ToIplImage()
+
+        Executors.newSingleThreadExecutor().execute {
+            while (true) {
+                val iplImage = grabberConverter.convert(grabber.grabFrame())
+                cvFlip(iplImage, iplImage, 1)
+                val frame = grabberConverter.convert(iplImage)
+                image.image = frameToImage(frame)
+            }
+        }
+    }
+
+    fun getImageWidth(): Double {
+        return grabber.grabFrame().imageWidth.toDouble()
+    }
+
+    private fun frameToImage(frame: Frame): WritableImage {
+        val bufferedImage: BufferedImage = java2DFrameConverter.getBufferedImage(frame)
+        return SwingFXUtils.toFXImage(bufferedImage, null)
+    }
+
+    init {
+        grabber.start()
+    }
+
+}
+
 class WebcamView : View("Matterialize") {
 
-    private val java2DFrameConverter: Java2DFrameConverter = Java2DFrameConverter()
+    private val webcamViewController: WebcamViewController by inject()
 
     override val root = stackpane {
-        val grabber = OpenCVFrameGrabber(0)
-        grabber.start()
-        setPrefSize(grabber.grabFrame().imageWidth.toDouble(), 150.0)
+        setPrefSize(webcamViewController.getImageWidth(), 150.0)
 
         val label = Label("No image selected")
 
         vbox {
             imageview {
-                val grabberConverter = OpenCVFrameConverter.ToIplImage()
-
-                Executors.newSingleThreadExecutor().execute {
-                    while (true) {
-                        val iplImage = grabberConverter.convert(grabber.grabFrame())
-                        cvFlip(iplImage, iplImage, 1)
-                        val frame = grabberConverter.convert(iplImage)
-                        this.image = frameToImage(frame)
-                    }
-                }
+                webcamViewController.initialiseWebCamThread(this)
             }
 
             borderpane {
@@ -55,6 +88,7 @@ class WebcamView : View("Matterialize") {
                     action {
                         val fileChooser = FileChooser()
                         val file = fileChooser.showOpenDialog(null)
+                        webcamViewController.applyNewBackground(file?.absolutePath ?: "err")
                         label.text = file?.name ?: label.text
                     }
                 }
@@ -71,11 +105,5 @@ class WebcamView : View("Matterialize") {
                 center = label
             }
         }
-
-    }
-
-    private fun frameToImage(frame: Frame): WritableImage {
-        val bufferedImage: BufferedImage = java2DFrameConverter.getBufferedImage(frame)
-        return SwingFXUtils.toFXImage(bufferedImage, null)
     }
 }
