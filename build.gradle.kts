884275f56142a35195a32f76812422c2cbc7f73b
Matteo Bilardi <matteo.bilardi18@imperial.ac.uk>
2020-10-16 13:52:54 +0100

    SEGP-13 Add testing suite and testcase for YUYV conversion

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,15 +14,23 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test-junit"))
     implementation("org.openpnp:opencv:4.3.0-2")
     implementation("com.github.haifengl:smile-kotlin:2.5.3")
     implementation("no.tornado:tornadofx:1.7.20")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.0-M1")
+    testImplementation("io.kotest:kotest-runner-junit5:4.3.0")
+    testImplementation("io.kotest:kotest-assertions-core:4.3.0")
+    testImplementation("io.kotest:kotest-property:4.3.0")
 }
 
 javafx {
     modules("javafx.controls", "javafx.fxml", "javafx.swing")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
 }
 
 tasks.withType<KotlinCompile>() {
