d1890205e541a2dcdff9b976163c29242032ec2a
Matteo Bilardi <matteo.bilardi18@imperial.ac.uk>
2020-10-23 19:51:50 +0100

    SEGP-35 Continue implementation of background cut models

--- src/main/kotlin/uk/ac/ic/matterialize/matting/BackgroundCut.kt
+++ src/main/kotlin/uk/ac/ic/matterialize/matting/BackgroundCut.kt
@@ -2,17 +2,21 @@ package uk.ac.ic.matterialize.matting
 
 import org.opencv.core.Core
 import org.opencv.core.CvType.CV_8UC1
-import org.opencv.core.CvType.CV_8UC3
 import org.opencv.core.Mat
 import org.opencv.core.Size
 import org.opencv.imgcodecs.Imgcodecs
+import org.opencv.imgproc.Imgproc
 import org.opencv.imgproc.Imgproc.blur
 import org.opencv.ml.EM
 import uk.ac.ic.matterialize.util.OpenCV
+import kotlin.math.PI
+import kotlin.math.exp
+import kotlin.math.pow
 
 private interface Model {
 
 }
+
 
 data class SegmentationResult(val image: Mat, val backgroundMask: Mat)
 
@@ -69,38 +73,55 @@ fun Mat.getPerPixelNeighborhoodVariances(): Mat {
     return result
 }
 
-private class BackgroundModel(private val backgroundImage: Mat) : Model {
+fun Mat.toSamples(): Mat {
+    return this.clone().reshape(1, this.width() * this.height())
+}
+
+typealias PixelPosition = Int
+val twoPiCubed = (2 * PI).pow(3)
+fun pdf(sample: Mat, mean: Mat, bppVar: Double): Double {
+    return exp(-1/(2 * bppVar * bppVar) * Core.norm(sample, mean)) / kotlin.math.sqrt(twoPiCubed * bppVar * bppVar * bppVar)
+}
+
+private class BackgroundModel(backgroundImage: Mat) : Model {
     private val gmm: EM = EM.create()
 
     // K_b in the background cut paper equation (2)
     // typical values in range 10-15
-    private val numComponents = 13
+    private val numComponents = 5
+    private val backgroundImageSamples = backgroundImage.toSamples()
 
     // alpha in equation (4)
     // must be between 0 and 1
     private val mixingFactor = 0.5
     private val perPixelNeighborhoodVariances: Mat
 
-
     init {
         assert(mixingFactor in 0.0..1.0)
         gmm.clustersNumber = numComponents
         // FIXME requires sample channels to be 1 so might need to convert image
-        gmm.trainEM(backgroundImage)
+        gmm.trainEM(backgroundImageSamples)
         perPixelNeighborhoodVariances = backgroundImage.getPerPixelNeighborhoodVariances()
     }
 
     // TODO Check if this is incorrect, but I'm pretty sure it does exactly equation (2)
     fun globalProbs(image: Mat): Mat {
         val result = Mat()
-        gmm.predict(image, result)
+        gmm.predict(image.toSamples(), result)
         return result
     }
 
     fun perPixelProbs(image: Mat): Mat {
-        val means = backgroundImage
-        TODO()
+        val samples = image.toSamples()
+        val probs = Mat(samples.size(), samples.type())
+        for (i in 0 until backgroundImageSamples.height()) {
+            val sample = samples.row(i)
+            val pixelVariance = perPixelNeighborhoodVariances.get(i, 0)[0]
+            val prob = pdf(sample, backgroundImageSamples.row(i), pixelVariance)
+            probs.put(i, 0, prob)
+        }
 
+        return probs
     }
 
     fun mixProbs(image: Mat): Mat {
@@ -116,15 +137,23 @@ private class BackgroundModel(private val backgroundImage: Mat) : Model {
 fun main() {
     OpenCV.load()
     // create simple 2x2 picture
-    val exampleMat = Mat(Size(2.0, 2.0), CV_8UC3)
-    val green = ubyteArrayOf(0u, 255u, 0u).toByteArray()
-    val red = ubyteArrayOf(0u, 0u, 255u).toByteArray()
-    val blue = ubyteArrayOf(255u, 0u, 0u).toByteArray()
-    val white = ubyteArrayOf(255u, 255u, 255u).toByteArray()
 
-    exampleMat.put(0, 0, green + red + blue + white)
+    val exampleMat = Imgcodecs.imread("/home/matteo/Downloads/COCO_train2014_000000000762.jpg")!!
+    Imgproc.resize(exampleMat, exampleMat, Size(640.0, 480.0))
+    Imgcodecs.imwrite("cache.png", exampleMat)
+
+    val samples = exampleMat.clone().reshape(1, exampleMat.width() * exampleMat.height())
+    //for (y in 0 until samples.rows()) {
+    //    print("[")
+    //    for (x in 0 until samples.cols()) {
+    //        print(samples.get(y, x).contentToString())
+    //    }
+    //    println("]")
+    //}
+    println(samples.channels())
 
     val globalBackgroundModel = BackgroundModel(exampleMat)
+    globalBackgroundModel.perPixelProbs(exampleMat)
 
-    Imgcodecs.imwrite("cache.png", exampleMat)
+
 }
