eb68c5126f3359f2927399b30ed1637ece7753b7
me@eugenel.in <me@eugenel.in>
2020-10-26 12:32:36 +0000

    Modify background cut with inline l2 norm and spherical covariance matrix

--- src/main/kotlin/uk/ac/ic/matterialize/matting/BackgroundCut.kt
+++ src/main/kotlin/uk/ac/ic/matterialize/matting/BackgroundCut.kt
@@ -4,6 +4,7 @@ import org.opencv.core.Core
 import org.opencv.core.CvType.CV_8UC1
 import org.opencv.core.Mat
 import org.opencv.core.Size
+import org.opencv.core.TermCriteria
 import org.opencv.imgcodecs.Imgcodecs
 import org.opencv.imgproc.Imgproc
 import org.opencv.imgproc.Imgproc.blur
@@ -56,7 +57,7 @@ private class PerPixelBackgroundModel(backgroundImage: Mat) : Model {
 }
 
 // TODO find a better place for this
-fun Mat.getPerPixelNeighborhoodVariances(): Mat {
+fun Mat.getPerPixelNeighborhoodVariances(): Array<DoubleArray> {
     val meanOfSquare = Mat()
     val mean = Mat()
     val result = Mat()
@@ -70,17 +71,30 @@ fun Mat.getPerPixelNeighborhoodVariances(): Mat {
 
     Core.subtract(meanOfSquare, squareOfMean, result)
 
-    return result
+    return result.toSamples().toArray()
 }
 
 fun Mat.toSamples(): Mat {
     return this.clone().reshape(1, this.width() * this.height())
 }
 
+fun Mat.toArray(): Array<DoubleArray> {
+    return (0 until this.height()).map { h ->
+        doubleArrayOf(
+            this.get(h, 0)[0],
+            this.get(h, 1)[0],
+            this.get(h, 2)[0]
+        )
+    }.toTypedArray()
+}
+
 typealias PixelPosition = Int
 val twoPiCubed = (2 * PI).pow(3)
-fun pdf(sample: Mat, mean: Mat, bppVar: Double): Double {
-    return exp(-1/(2 * bppVar * bppVar) * Core.norm(sample, mean)) / kotlin.math.sqrt(twoPiCubed * bppVar * bppVar * bppVar)
+fun pdf(l2: Double, bppVar: Double): Double {
+    // return exp(-1/(2 * bppVar * bppVar) * Core.norm(sample, mean)) / kotlin.math.sqrt(twoPiCubed * bppVar * bppVar * bppVar)
+    return 1 / kotlin.math.sqrt(
+        (twoPiCubed * bppVar * bppVar * exp(2 * bppVar * bppVar * l2))
+    )
 }
 
 private class BackgroundModel(backgroundImage: Mat) : Model {
@@ -90,14 +104,24 @@ private class BackgroundModel(backgroundImage: Mat) : Model {
     // typical values in range 10-15
     private val numComponents = 5
     private val backgroundImageSamples = backgroundImage.toSamples()
+    private val backgroundImageSamplesArray = backgroundImageSamples.toArray()
 
     // alpha in equation (4)
     // must be between 0 and 1
     private val mixingFactor = 0.5
-    private val perPixelNeighborhoodVariances: Mat
+    private val perPixelNeighborhoodVariances: Array<DoubleArray>
 
     init {
         assert(mixingFactor in 0.0..1.0)
+
+        gmm.termCriteria = TermCriteria(
+            gmm.termCriteria.type,
+            10,
+            gmm.termCriteria.epsilon
+        )
+
+        gmm.covarianceMatrixType = EM.COV_MAT_SPHERICAL
+
         gmm.clustersNumber = numComponents
         // FIXME requires sample channels to be 1 so might need to convert image
         gmm.trainEM(backgroundImageSamples)
@@ -113,11 +137,15 @@ private class BackgroundModel(backgroundImage: Mat) : Model {
 
     fun perPixelProbs(image: Mat): Mat {
         val samples = image.toSamples()
+
         val probs = Mat(samples.size(), samples.type())
         for (i in 0 until backgroundImageSamples.height()) {
-            val sample = samples.row(i)
-            val pixelVariance = perPixelNeighborhoodVariances.get(i, 0)[0]
-            val prob = pdf(sample, backgroundImageSamples.row(i), pixelVariance)
+            val d0 = samples.get(i, 0)[0] - backgroundImageSamplesArray[i][0]
+            val d1 = samples.get(i, 1)[0] - backgroundImageSamplesArray[i][1]
+            val d2 = samples.get(i, 2)[0] - backgroundImageSamplesArray[i][2]
+            val l2 = d0 * d0 + d1 * d1 + d2 * d2
+            val pixelVariance = perPixelNeighborhoodVariances[i][0]
+            val prob = pdf(l2, pixelVariance)
             probs.put(i, 0, prob)
         }
 
@@ -138,8 +166,8 @@ fun main() {
     OpenCV.load()
     // create simple 2x2 picture
 
-    val exampleMat = Imgcodecs.imread("/home/matteo/Downloads/COCO_train2014_000000000762.jpg")!!
-    Imgproc.resize(exampleMat, exampleMat, Size(640.0, 480.0))
+    val exampleMat = Imgcodecs.imread("/home/line/usr/doc/ic/assignments/3xx_60021_segp/adobe/bgs/train2014/COCO_train2014_000000000762.jpg")!!
+    Imgproc.resize(exampleMat, exampleMat, Size(320.0, 240.0))
     Imgcodecs.imwrite("cache.png", exampleMat)
 
     val samples = exampleMat.clone().reshape(1, exampleMat.width() * exampleMat.height())
@@ -152,8 +180,14 @@ fun main() {
     //}
     println(samples.channels())
 
+    val t0 = System.currentTimeMillis()
     val globalBackgroundModel = BackgroundModel(exampleMat)
+    val t1 = System.currentTimeMillis()
     globalBackgroundModel.perPixelProbs(exampleMat)
+    val t2 = System.currentTimeMillis()
+
+    println("t1 - t0: ${t1 - t0}ms")
+    println("t2 - t1: ${t2 - t1}ms")
 
 
 }
