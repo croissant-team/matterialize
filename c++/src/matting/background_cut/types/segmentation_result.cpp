a997127006a752f28eb758c25b0af3789d83cb27
Devesh Erda <devesh@erda.dev>
2020-10-31 21:34:20 +0000

    Convert segmentation result to C++

--- c++/src/matting/background_cut/types/segmentation_result.cpp
+++ c++/src/matting/background_cut/types/segmentation_result.cpp
@@ -1,2 +1,14 @@
 #include "segmentation_result.hpp"
+#include "flat_mask.hpp"
+#include "image.hpp"
 
+#include <utility>
+
+#include <opencv2/core.hpp>
+
+SegmentationResult SegmentationResult::empty(int rows, int cols) {
+  Image image(cv::Mat::zeros(rows, cols, CV_8UC3));
+  FlatMask bg_mask(cv::Mat::zeros(rows * cols, 1, CV_8UC1));
+
+  return SegmentationResult(std::move(image), std::move(bg_mask));
+}
--- c++/src/matting/background_cut/types/segmentation_result.hpp
+++ c++/src/matting/background_cut/types/segmentation_result.hpp
@@ -1,4 +1,23 @@
 #ifndef MATTERIALIZE_SEGMENTATION_RESULT_HPP
 #define MATTERIALIZE_SEGMENTATION_RESULT_HPP
 
+#include "flat_mask.hpp"
+#include "image.hpp"
+
+#include <utility>
+
+#include <opencv2/core.hpp>
+
+class SegmentationResult {
+private:
+  const Image image;
+  const FlatMask bg_mask;
+
+  SegmentationResult(Image &&image, FlatMask &&bg_mask) noexcept
+      : image{std::move(image)}, bg_mask{std::move(bg_mask)} {}
+
+public:
+  static SegmentationResult empty(int rows, int cols);
+};
+
 #endif
