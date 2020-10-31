287971633937bf4703b1ab0abed212fde24c2583
Devesh Erda <devesh@erda.dev>
2020-10-31 00:05:25 +0000

    Convert opencv matter to C++

---
+++ c++/src/matting/opencv_matter.cpp
@@ -0,0 +1,11 @@
+#include "opencv_matter.hpp"
+
+#include <opencv2/core.hpp>
+
+cv::Mat OpenCVMatter::background_mask(const cv::Mat &video_frame) {
+  cv::Mat mask{};
+
+  background_subtractor->apply(video_frame, mask);
+
+  return mask;
+}
---
+++ c++/src/matting/opencv_matter.hpp
@@ -0,0 +1,17 @@
+#ifndef MATTERIALIZE_OPENCV_MATTER_HPP
+#define MATTERIALIZE_OPENCV_MATTER_HPP
+
+#include "matter.hpp"
+
+#include <opencv2/core.hpp>
+#include <opencv2/video.hpp>
+
+class OpenCVMatter : public IMatter {
+private:
+  cv::Ptr<cv::BackgroundSubtractor> background_subtractor{cv::createBackgroundSubtractorKNN()};
+
+public:
+  cv::Mat background_mask(const cv::Mat &video_frame) override;
+};
+
+#endif
