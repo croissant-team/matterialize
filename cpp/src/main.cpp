7af58347ede04cc0a708259a12db5cfe2779b98d
Andreas Casapu <ac9018@ic.ac.uk>
2020-11-02 18:43:21 +0000

    SEGP-67 WIP Add v4l2 c++ wrapper library to project and CMakeLists and begin implementing the real cam - fake cam pipeline

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -3,11 +3,13 @@
 #include <iostream>
 
 #include <opencv2/highgui.hpp>
+#include <v4l2cpp/V4l2Capture.h>
+#include <v4l2cpp/V4l2Device.h>
 
 int main() {
   std::cout << "Matterialize\n";
 
-  OpenCVWebcam webcam(0, 1280, 720);
+  /*OpenCVWebcam webcam(0, 1280, 720);
 
   webcam.start();
 
@@ -26,5 +28,10 @@ int main() {
     if (cv::waitKey(10) == 27) {
       break;
     }
-  }
+  }*/
+
+  V4L2DeviceParameters param(
+      "/dev/video0", V4L2_PIX_FMT_YUYV, 640, 480, 25, true);
+  V4l2Capture *videoCapture =
+      V4l2Capture::create(param, V4l2Access::IOTYPE_MMAP);
 }
