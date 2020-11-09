ba23485471d32950fd000449682cdb75e990537a
Andreas Casapu <ac9018@ic.ac.uk>
2020-11-09 18:02:49 +0000

    Remove build directory from tracking

--- build/CMakeFiles/clang-format-project-files.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clang-format-project-files"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clang-format-project-files.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/matterialize_tests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for matterialize_tests.
-# This may be replaced when dependencies are built.
