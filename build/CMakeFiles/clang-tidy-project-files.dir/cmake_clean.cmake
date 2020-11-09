b27e4ea3e8b77f7986bcf1126a4deb5a3b612f25
Andreas Casapu <ac9018@ic.ac.uk>
2020-11-09 17:53:31 +0000

    SEGP-68 Remove kotlin project

---
+++ build/CMakeFiles/clang-tidy-project-files.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clang-tidy-project-files"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/clang-tidy-project-files.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main/kotlin/uk/ac/ic/matterialize/Main.kt
+++
@@ -1,8 +0,0 @@
-package uk.ac.ic.matterialize
-
-import tornadofx.launch
-import uk.ac.ic.matterialize.gui.MatterializeApp
-
-fun main() {
-    launch<MatterializeApp>()
-}
